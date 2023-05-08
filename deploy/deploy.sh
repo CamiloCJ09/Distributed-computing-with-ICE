5762f615f03ef40832c7c66ec149d93b534b0255
Johan Stiven Ricardo <johan.ricardo1104@gmail.com>
2023-05-08 09:22:47 -0500

    feat: start for deployment and set for de ThreadPool server size using bash

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+clients=$(cat clients.txt | wc -l)
+cd ..
+gradle clean
+gradle build
+
+
+echo "Deploy Server"
+#Set de amount of clients 
+sed -i "s/#Ice.ThreadPool.Server.Size=n/Ice.ThreadPool.Server.Size=$clients/g" server/src/main/resources/config.server
+cd server/build/libs || exit
+sshpass -p "swarch" scp -o StrictHostKeyChecking=no -r server.jar swarch@10.147.19.158:/home/swarch/johanRicardo-CamiloCampaz/callback-server-deploy
+sshpass -p "swarch" scp -o StrictHostKeyChecking=no -r ice-3.7.6.jar swarch@10.147.19.158:/home/swarch/johanRicardo-CamiloCampaz/callback-server-deploy
+
+cd ../..
+echo "Deploy Clients"
+
+#TODO: implement reading for all the clients choosen. read from clients.txt file
+
+#TODO: create folder in clients machines 
